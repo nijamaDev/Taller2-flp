9e2a0daf9db832d3d571b810c23930dbbc47638c
kiin The Dovah <kiinthedovah@gmail.com>
2021-03-24 08:56:51 -0500

    difftree completed

--- difftree.rkt
+++ difftree.rkt
@@ -11,6 +11,7 @@
     (cond
       [(equal? l (diff-one)) #t]
       [(and (diff-one? (cadr l)) (diff-zero? (caddr l))) #t]
+      [(and (diff-zero? (cadr l)) (diff-negOne? (caddr l))) #t]
       [else #f]
       )
     )
@@ -18,7 +19,7 @@
 
 (define diff-zero
   (lambda ()
-    '(diff (diff-one) (diff-one))
+    (list 'diff (diff-one) (diff-one))
     )
   )
 
@@ -44,6 +45,7 @@
 (define diff-negOne?
   (lambda (l)
     (cond
+      [(equal? l (diff-negOne)) #t]
       [(or (diff-one? l) (diff-zero? l)) #f]
       [(and (diff-zero? (cadr l)) (diff-one? (caddr l)) ) #t]
       [(and (diff-negOne? (cadr l)) (diff-zero? (caddr l)) ) #t]
@@ -52,11 +54,36 @@
     )
   )
 
+(define diff-successor
+  (lambda (l)
+    (diff (list 'diff l (diff-negOne)))
+    )
+  )
+
+(define diff-predecessor
+  (lambda (l)
+    (diff (list 'diff l (diff-one)))
+    )
+  )
+
 (define diff
   (lambda (l)
     (cond
       [(diff-one? l) (diff-one)]
-      ;[()]
+      [(diff-zero? l) (diff-zero)]
+      [(diff-negOne? l) (diff-negOne)]
+      [else l]
+      )
+    )
+  )
+
+(define diff-tree-plus
+  (lambda (l)
+    (cond
+      [(diff-one? l) 1]
+      [(diff-zero? l) 0]
+      [(diff-negOne? l) -1]
+      [else (- (diff-tree-plus (cadr l)) (diff-tree-plus (caddr l)))]
       )
     )
   )
